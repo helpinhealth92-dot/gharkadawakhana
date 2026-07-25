2785b25fc725a3e73c9d4058832aee1d17c794aa
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-25 22:56:50 +0500

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+export default function Home(){
+
+return (
+
+<main>
+
+<h1>
+گھر کا دواخانہ
+</h1>
+
+<p>
+قدرتی صحت اور حکمت کی مصنوعات
+</p>
+
+<a href="/products">
+مصنوعات دیکھیں
+</a>
+
+</main>
+
+);
+
+}
