b53a64ec6c559fe8eed5453a3855f9adaaeaa731
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 01:38:18 +0500

    Update CartContext.jsx

--- app/app/context/CartContext.jsx
+++ app/app/context/CartContext.jsx
@@ -4,61 +4,32 @@ import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
-
 export function CartProvider({ children }) {
 
   const [cart, setCart] = useState([]);
 
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
 
-  function addToCart(product) {
-
-    setCart([
-      ...cart,
-      product
-    ]);
-
-  }
-
-
-  function removeFromCart(index) {
-
-    setCart(
-      cart.filter((item, i) => i !== index)
-    );
-
-  }
-
-
-  function clearCart() {
-
-    setCart([]);
-
-  }
-
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index));
+  };
 
   return (
-
     <CartContext.Provider
       value={{
         cart,
         addToCart,
-        removeFromCart,
-        clearCart
+        removeFromCart
       }}
     >
-
       {children}
-
     </CartContext.Provider>
-
   );
-
 }
 
 
-
 export function useCart() {
-
   return useContext(CartContext);
-
 }
