d830996d65e137fda779d5b0a1390912a31b9151
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 01:31:17 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,19 @@
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
 
-
 export const metadata = {
   title: "گھر کا دَواخانہ",
   description: "قدرتی جڑی بوٹیاں اور ہربل مصنوعات",
 };
 
-
 export default function RootLayout({ children }) {
-
   return (
-
     <html lang="ur" dir="rtl">
-
       <body>
-
         <CartProvider>
           {children}
         </CartProvider>
-
       </body>
-
     </html>
-
   );
-
 }
