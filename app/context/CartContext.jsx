86d706a3b5ba2835d0e3c491135f87d1f08ed8bf
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 03:36:14 +0500

    Create CartContext.jsx

---
+++ app/context/CartContext.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((oldCart) => [...oldCart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    setCart((oldCart) =>
+      oldCart.filter((_, i) => i !== index)
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
