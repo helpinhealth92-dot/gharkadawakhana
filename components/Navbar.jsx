35101c65dd450f8b0dc515864f2ed76a0fbc8698
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 07:12:01 +0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,26 +8,112 @@ export default function Navbar() {
   const { cart } = useCart();
 
   return (
+
     <nav
       style={{
-        padding: "15px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        background: "#1b5e20",
+        color: "white",
+        padding: "12px 20px",
         direction: "rtl"
       }}
     >
 
-      <h2>
-        گھر کا دَواخانہ
-      </h2>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexWrap: "wrap"
+        }}
+      >
+
+        {/* Logo + Name */}
+
+        <Link
+          href="/"
+          style={{
+            color: "white",
+            textDecoration: "none"
+          }}
+        >
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "10px"
+            }}
+          >
+
+            <img
+              src="/logo.png"
+              alt="گھر کا دَواخانہ"
+              width="55"
+              height="55"
+              style={{
+                borderRadius: "50%",
+                objectFit: "cover"
+              }}
+            />
 
 
-      <Link href="/cart">
-        🛒 کارٹ ({cart.length})
-      </Link>
+            <h2>
+              🌿 گھر کا دَواخانہ
+            </h2>
+
+          </div>
+
+        </Link>
+
+
+
+        {/* Menu */}
+
+        <div>
+
+          <Link
+            href="/"
+            style={{
+              color:"white",
+              margin:"8px",
+              textDecoration:"none"
+            }}
+          >
+            ہوم
+          </Link>
+
+
+          <Link
+            href="/products"
+            style={{
+              color:"white",
+              margin:"8px",
+              textDecoration:"none"
+            }}
+          >
+            مصنوعات
+          </Link>
+
+
+          <Link
+            href="/cart"
+            style={{
+              color:"white",
+              margin:"8px",
+              textDecoration:"none"
+            }}
+          >
+            🛒 کارٹ ({cart.length})
+          </Link>
+
+
+        </div>
+
+
+      </div>
 
 
     </nav>
+
   );
 }
