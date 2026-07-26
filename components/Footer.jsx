cec938a17c56817ee9f3e6918da1292590970243
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 05:48:59 +0500

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,40 @@
+export default function Footer(){
+
+  return (
+
+    <footer
+      style={{
+        background:"#1b5e20",
+        color:"white",
+        padding:"25px",
+        textAlign:"center",
+        direction:"rtl",
+        marginTop:"40px"
+      }}
+    >
+
+      <h3>
+        🌿 گھر کا دَواخانہ
+      </h3>
+
+
+      <p>
+        قدرتی جڑی بوٹیاں اور ہربل مصنوعات
+      </p>
+
+
+      <p>
+        WhatsApp: 03477357397
+      </p>
+
+
+      <p>
+        © تمام حقوق محفوظ ہیں
+      </p>
+
+
+    </footer>
+
+  );
+
+}
