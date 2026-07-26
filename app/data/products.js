3d42b5e055193cf7504a5f308f8b79662529cad5
helpinhealth92-dot <helpinhealth92@gmail.com>
2026-07-26 18:29:33 +0500

    Create products.js

---
+++ app/data/products.js
@@ -0,0 +1,116 @@
+const products = [
+
+  {
+    id: 1,
+    urduName: "ہلدی پاؤڈر",
+    englishName: "Turmeric Powder",
+    description: "خالص قدرتی ہلدی پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 2,
+    urduName: "لہسن پاؤڈر",
+    englishName: "Garlic Powder",
+    description: "قدرتی لہسن پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 3,
+    urduName: "ادرک پاؤڈر",
+    englishName: "Ginger Powder",
+    description: "خالص ادرک پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 4,
+    urduName: "دارچینی پاؤڈر",
+    englishName: "Cinnamon Powder",
+    description: "اعلیٰ معیار دارچینی پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 5,
+    urduName: "زیرہ پاؤڈر",
+    englishName: "Cumin Powder",
+    description: "خالص زیرہ پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 6,
+    urduName: "کلونجی پاؤڈر",
+    englishName: "Black Seed Powder",
+    description: "قدرتی کلونجی پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 7,
+    urduName: "آملہ پاؤڈر",
+    englishName: "Amla Powder",
+    description: "قدرتی آملہ پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 8,
+    urduName: "اشوگندھا پاؤڈر",
+    englishName: "Ashwagandha Powder",
+    description: "قدرتی جڑی بوٹی پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 9,
+    urduName: "سہانجنا پاؤڈر",
+    englishName: "Moringa Powder",
+    description: "خالص سہانجنا پاؤڈر",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  },
+
+
+  {
+    id: 10,
+    urduName: "اسپغول کا چھلکا",
+    englishName: "Psyllium Husk",
+    description: "قدرتی اسپغول",
+    image: "",
+    weight: "1 کلو",
+    price: "3000 روپے"
+  }
+
+
+];
+
+
+export default products;
